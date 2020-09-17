5449a4c3bbb73d37ec6af8498b2f77481251419b
Xamarsia <xamarsia@gmail.com>
2020-09-17 16:33:48 +0300

    refactor: correct minor typos in code

--- task.py
+++ task.py
@@ -3,13 +3,11 @@
 
 from sqlalchemy import create_engine
 from datetime import datetime, timedelta
-
-engine = create_engine('sqlite:///todo.db?check_same_thread=False')
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
 from datetime import datetime
 
+engine = create_engine('sqlite:///todo.db?check_same_thread=False')
 Base = declarative_base()
 
 
@@ -19,7 +17,7 @@ class Table(Base):
                 primary_key=True)  # id - целочисленный столбец таблицы; primary_key=True говорит, что этот столбец является первичным ключом.
     task = Column(String)  # task - это строковый столбец;
     deadline = Column(Date,
-                      default=datetime.today())  # deadline - это столбец, в котором хранится дата. SQLAlchemy автоматически преобразует дату SQL в объект date и времени Python.
+                      default=datetime.today())
 
     def __repr__(self):
         return self.task
@@ -27,8 +25,6 @@ class Table(Base):
 
 # После того, как мы описали нашу таблицу, самое время создать ее в нашей базе данных. Все, что нам нужно, это вызвать метод create_all () и передать ему движок:
 Base.metadata.create_all(engine)
-# Этот метод создает таблицу в нашей базе данных путем генерации SQL-запросов в соответствии с описанными нами моделями.
-# Теперь мы можем получить доступ к базе данных и хранить в ней данные. Для доступа к базе данных нам нужно создать сеанс:
 
 from sqlalchemy.orm import sessionmaker
 
@@ -36,71 +32,29 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-rows = session.query(Table).all()  # Метод all () возвращает все строки из таблицы в виде списка Python.
+rows = session.query(Table).all()
 
 status = True
-while status:  # роботает
+while status:
     act = input(
         "\n1) Today's tasks\n2) Week's tasks\n3) All tasks\n4) Missed tasks\n5) Add task\n6) Delete task\n0) Exit")
     if act == "0":
         status = False
         print("\nBye!")
 
-    elif act == "3":  # роботает
-        rows = session.query(Table).order_by(Table.deadline).all()
+    elif act == "1":
+        date_time = datetime.today().date()  # текущая дата без времени
+        print(date_time.strftime("\nToday " "%d %b" ":"))
+        today = datetime.today()
+        rows = session.query(Table).filter(Table.deadline == today.date()).all()  # вибрать все строки по дате
         ro = [i for i in range(1, len(rows) + 1)]
         i = 0
-        print("\nAll tasks:")
         for row in rows:
             if len(rows) != 0:
-                print(str(ro[i]) + ".", str(row) + ".", datetime.strftime(row.deadline,
-                                                                          "%d %b"))  # Распечатает строку, возвращенную методом __repr__, a именно task
+                print(str(ro[i]) + ".", row)
                 i += 1
-
-    elif act == "4":  # го тово
-        # Когда вы печатаете пропущенное задание, вы не распечатываете их все !.
-        rows = session.query(Table).filter(Table.deadline < datetime.today().date()).all()
-        today = datetime.today().date()
-        ro = [i for i in range(1, len(rows) + 1)]
-        i = 0
-        print("\nMissed tasks:")
-        for row in rows:
-            if row.deadline < today:
-                if len(rows) != 0:
-                    print(str(ro[i]) + ".", str(row) + ".", datetime.strftime(row.deadline,
-                                                                              "%d %b"))  # Распечатает строку, возвращенную методом __repr__, a именно task
-                    i += 1
         if len(rows) == 0:
-            print("Nothing is missed!")
-
-    elif act == "6":
-        rows = session.query(Table).order_by(Table.deadline).all()
-        ro = [i for i in range(1, len(rows) + 1)]
-        i = 0
-        print("\nChoose the number of the task you want to delete:")
-        for row in rows:
-            if len(rows) != 0:
-                print(str(ro[i]) + ".", str(row) + ".", datetime.strftime(row.deadline,
-                                                                          "%d %b"))  # Распечатает строку, возвращенную методом __repr__, a именно task
-                i += 1
-
-        deleted_task = int(input())
-        print(rows[0])  # sncsdfvufgdtsfdcts выводит одно значение
-        specific_row = rows[deleted_task - 1]  # in case rows is not empty
-        session.delete(specific_row)
-        session.commit()
-
-        print("The task has been deleted!")
-
-        pass
-
-    elif act == "5":  # роботает
-        add_task = input("\nEnter task")
-        deadline = input("Enter deadline")  # 2020-04-28
-        new_row = Table(task=add_task, deadline=datetime.strptime(deadline, '%Y-%m-%d'))
-        session.add(new_row)
-        session.commit()
-        print("The task has been added!")
+            print("Nothing to do!")
 
     elif act == "2":
         today = datetime.today()
@@ -123,16 +77,51 @@ while status:  # роботает
             day = 1
             counter += 1
 
-    elif act == "1":
-        date_time = datetime.today().date()  # текущая дата без времени
-        print(date_time.strftime("\nToday " "%d %b" ":"))
-        today = datetime.today()
-        rows = session.query(Table).filter(Table.deadline == today.date()).all()  # вибрать все строки по дате
+    elif act == "3":
+        rows = session.query(Table).order_by(Table.deadline).all()
         ro = [i for i in range(1, len(rows) + 1)]
         i = 0
+        print("\nAll tasks:")
         for row in rows:
             if len(rows) != 0:
-                print(str(ro[i]) + ".", row)  # Распечатает строку, возвращенную методом __repr__, a именно task
+                print(str(ro[i]) + ".", str(row) + ".", datetime.strftime(row.deadline, "%d %b"))
                 i += 1
+
+    elif act == "4":
+        rows = session.query(Table).filter(Table.deadline < datetime.today().date()).all()
+        today = datetime.today().date()
+        ro = [i for i in range(1, len(rows) + 1)]
+        i = 0
+        print("\nMissed tasks:")
+        for row in rows:
+            if row.deadline < today:
+                if len(rows) != 0:
+                    print(str(ro[i]) + ".", str(row) + ".", datetime.strftime(row.deadline, "%d %b"))
+                    i += 1
         if len(rows) == 0:
-            print("Nothing to do!")
+            print("Nothing is missed!")
+
+    elif act == "5":
+        add_task = input("\nEnter task")
+        deadline = input("Enter deadline")  # 2020-04-28
+        new_row = Table(task=add_task, deadline=datetime.strptime(deadline, '%Y-%m-%d'))
+        session.add(new_row)
+        session.commit()
+        print("The task has been added!")
+
+    elif act == "6":
+        rows = session.query(Table).order_by(Table.deadline).all()
+        ro = [i for i in range(1, len(rows) + 1)]
+        i = 0
+        print("\nChoose the number of the task you want to delete:")
+        for row in rows:
+            if len(rows) != 0:
+                print(str(ro[i]) + ".", str(row) + ".", datetime.strftime(row.deadline, "%d %b"))
+                i += 1
+        deleted_task = int(input())
+        print(rows[0])
+        specific_row = rows[deleted_task - 1]
+        session.delete(specific_row)
+        session.commit()
+
+        print("The task has been deleted!")
